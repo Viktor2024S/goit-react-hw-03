c8a2a37b291ee1b8264e933c02b57f9f601df43e
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-20 18:40:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import { useState } from "react";
 import "./App.module.css";
-import ContactForm from "./componenets/ContactForm/ContactForm";
-import SearchBox from "./componenets/SearchBox/SearchBox";
-import ContactList from "./componenets/ContactList/ContactList";
+// import ContactForm from "./componenets/ContactForm/ContactForm";
+// import SearchBox from "./componenets/SearchBox/SearchBox";
+// import ContactList from "./componenets/ContactList/ContactList";
 
 export default function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      {/* <ContactForm />
       <SearchBox />
-      <ContactList />
+      <ContactList /> */}
     </div>
   );
 }

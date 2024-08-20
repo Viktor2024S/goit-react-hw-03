e524eeedd07d1489bb6db02a5d4a493285751fd3
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-21 00:08:27 +0100

    upd styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,8 +3,13 @@ import styles from "./Contact.module.css";
 export default function Contact({ contact, deleteContact }) {
   return (
     <li className={styles.contact}>
-      {contact.name}: {contact.number}
-      <button onClick={() => deleteContact(contact.id)}>Delete</button>
+      <div className={styles.data}>
+        <span>{contact.name}:</span>
+        <span>{contact.number}</span>
+      </div>
+      <button onClick={() => deleteContact(contact.id)} className={styles.btn}>
+        Delete
+      </button>
     </li>
   );
 }
